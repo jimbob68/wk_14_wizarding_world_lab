cd01da5196a33100bb722356d6e46759c2053cdc
James Fraser <jimbob4t@virginmedia.com>
2020-03-03 16:04:43 +0000

    getting info to localhost

---
+++ src/containers/CharacterContainer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class CharacterContainer extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+
+    }
+  }
+  render(){
+    return (
+      <article>
+      <h1>Hooray for Hogwarts!!!</h1>
+      </article>
+    );
+  }
+}
+
+export default CharacterContainer;
