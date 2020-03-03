f0b01d0330fd9eaa7389afd02f1ec959f8943fc5
James Fraser <jimbob4t@virginmedia.com>
2020-03-03 16:23:40 +0000

    getting info from api

--- src/containers/CharacterContainer.js
+++ src/containers/CharacterContainer.js
@@ -4,8 +4,18 @@ class CharacterContainer extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-
+      characters: [],
+      selectedCharacterName: ''
     }
+  }
+
+  componentDidMount(){
+    const url = 'http://hp-api.herokuapp.com/api/characters'
+
+    fetch(url)
+    .then(res => res.json())
+    .then(characters => this.setState({characters: characters}))
+    .catch(err => console.error);
   }
   render(){
     return (
